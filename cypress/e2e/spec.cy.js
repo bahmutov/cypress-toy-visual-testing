97eea45b28b6e185749335434344076ba7dd0d3f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-02 09:39:21 -0400

    feat: add buttons

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,10 +12,21 @@ it(
   { viewportWidth: 300, viewportHeight: 100 },
   () => {
     cy.get('body')
-      .invoke('html', '<h1>Hello, world 1!</h1>')
+      .invoke('html', '<h1>Hello, world 2!</h1>')
       .invoke('addClass', 'foo')
     cy.imageDiff('no-classes')
     // if you change the text on the page and the diff fails
     // the screenshot "body" should not have the 'foo' class
+  },
+)
+
+it.skip(
+  'shows buttons on failure',
+  { viewportWidth: 700, viewportHeight: 400 },
+  () => {
+    cy.get('body')
+      .invoke('html', '<h1>Hello, world 5!</h1>')
+      .invoke('addClass', 'foo')
+    cy.imageDiff('buttons on failure')
   },
 )
